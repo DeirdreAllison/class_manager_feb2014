1f4e3551a4f46c5972aaf9ee20e3d083b4e7161e
Deirdre Allison <deirdre9@gmail.com>
2014-02-17 20:32:49 -0800

    added registration model for join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218035439) do
+ActiveRecord::Schema.define(version: 20140218043144) do
 
   create_table "courses", force: true do |t|
     t.integer  "student_id"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20140218035439) do
     t.date     "end_date"
     t.string   "location"
     t.string   "quarter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "registrations", force: true do |t|
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.string   "registration_number"
+    t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
