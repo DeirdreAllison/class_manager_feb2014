ccacd946ba80ec64d181283067da9abee33f0a28
Deirdre Allison <deirdre9@gmail.com>
2014-03-12 20:30:16 -0700

    fix our assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_student!
 
+  before_filter :configure_permitted_parameters, if:devise_controller?
+
+  protected
+
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) << :full_name
+
+  end
+
+
 end
