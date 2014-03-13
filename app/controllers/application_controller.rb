89a3a9c07210e2cbf0d321ecf3084d43ee9ec0c1
Deirdre Allison <deirdre9@gmail.com>
2014-03-12 21:25:08 -0700

    fixing the devise errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) << :full_name
+  	#devise_parameter_sanitizer.for(:sign_up) << :full_name
 
   end
 
